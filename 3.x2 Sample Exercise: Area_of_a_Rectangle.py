3d156f4e2806eb115f1be2a4ce68f42f23e36273
Sriram Selvaperumal <sriramselvaperumal@icloud.com>
2025-01-16 13:01:21 +0530

    Created 3.x2 Sample Exercise: Area_of_a_Rectangle.py

---
+++ 3.x2 Sample Exercise: Area_of_a_Rectangle.py
@@ -0,0 +1,8 @@
+# Basic Program to caluculate the Area of A Rectangle
+
+length = int(input("Enter The Length of the Rectangle: "))
+width = int(input("Enter The Lenth of the Rectangle: "))
+
+Area = length*width
+
+print("The Area of The Given Rectangle is",Area,"Cm^2")
