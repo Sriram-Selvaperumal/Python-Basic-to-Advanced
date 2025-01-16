9c99ea2a98b500c3ace8567965aec9ea44a6c892
Sriram Selvaperumal <sriramselvaperumal@icloud.com>
2025-01-16 12:53:19 +0530

    Created Sample Exercise : Basic_Input.py

---
+++ Sample Exercise : Basic_Input.py
@@ -0,0 +1,7 @@
+name = input("Please Enter Your Name:")
+age = int(input("Please Enter Your Age:"))
+gpa = float(input("Please Enter Your GPA:"))
+
+print("Name: ",name)
+print("Age: ",age)
+print("GPA: ",gpa)
